da66e3b703ebea7dc12ccba65265f0819da8f68a
Slava Belliaev <Viacheslav.Beliaev@workflowwise.com>
2020-10-30 23:33:54 +0200

    29. category api part one

--- controllers/category.js
+++ controllers/category.js
@@ -1,14 +1,55 @@
-module.exports.getAll = function(req, res) {
+const Category = require('../models/Category');
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
+
+module.exports.getAll = async function(req, res) {
+    try {
+        const categories = await Category.find({user: req.user.id});
+        res.status(200).json(categories);
+    }
+    catch(e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.getById = function(req, res) {
+module.exports.getById = async function(req, res) {
+    try {
+        const category = await Category.findById(req.params.id);
+        res.status(200).json(category);
+    }
+    catch(e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.remove = function(req, res) {
+module.exports.remove = async function(req, res) {
+    try {
+        const categoryId = req.params.id
+        await Category.remove({_id: categoryId});
+        await Position.remove({category: categoryId});
+        res.status(200).json({
+            message: "Category and related positions have been deleted"
+        });
+    }
+    catch(e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.create = function(req, res) {
+module.exports.create = async function(req, res) {
+    try {
+
+    }
+    catch(e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.update = function(req, res) {
+module.exports.update = async function(req, res) {
+    try {
+
+    }
+    catch(e) {
+        errorHandler(res, e)
+    }
 }

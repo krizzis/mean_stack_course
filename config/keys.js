646f81c55246c611db5de3685a18f5eb23f28be9
Slava Belliaev <Viacheslav.Beliaev@workflowwise.com>
2020-10-30 22:15:22 +0200

    23. login and token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://dn301182bva:EXua8ups1@cluster0.w9ngm.mongodb.net/meanappdb?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://dn301182bva:EXua8ups1@cluster0.w9ngm.mongodb.net/meanappdb?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,40 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
 const User = require('../models/User');
 
-module.exports.login = function(req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
+module.exports.login = async function(req, res) {
+    const userEmail = req.body.email;
+    const userPassword = req.body.password;
+    const user = await User.findOne({email: userEmail});
+
+    if (user) {
+        // Check user password
+        const passwordResult = bcrypt.compareSync(userPassword, user.password);
+        if (passwordResult) {
+            // Create a new token
+            const token = jwt.sign({
+                email: user.email,
+                userId: user._id
+            }, keys.jwt, {expiresIn: 60 * 60});
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            });
         }
-    })
+        else {
+            // Password is not matched - throw an error
+            res.status(401).json({
+                message: "User with such combination of email and password is not found"
+            });
+        }
+    }
+    else {
+        // User has not been found - throw an error
+        res.status(401).json({
+            message: "User with such combination of email and password is not found"
+        })
+    }
 }
 
 module.exports.register = async function(req, res) {

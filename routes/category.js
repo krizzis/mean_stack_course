1b07333d06a0f61c72fe89edfbad3aeaf19ace0c
Slava Belliaev <Viacheslav.Beliaev@workflowwise.com>
2020-10-31 22:28:14 +0200

    Add auth middleware

--- routes/category.js
+++ routes/category.js
@@ -5,8 +5,8 @@ const controller = require('../controllers/category');
 const router = express.Router()
 
 router.get('/', passport.authenticate('jwt', {session: false}), controller.getAll);
-router.get('/:id', controller.getById);
-router.delete('/:id', controller.remove);
+router.get('/:id', passport.authenticate('jwt', {session: false}), controller.getById);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), controller.remove);
 router.post('/', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.create);
 router.patch('/:id', upload.single('image'), controller.update);
 
--- routes/position.js
+++ routes/position.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const passport = require('passport');
 const controller = require('../controllers/position');
 const router = express.Router()
 
-router.get('/:categoryId', controller.getByCategoryId);
-router.delete('/:id', controller.remove);
-router.post('/', controller.create);
-router.patch('/:id', controller.update);
+router.get('/:categoryId', passport.authenticate('jwt', {session: false}), controller.getByCategoryId);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), controller.remove);
+router.post('/', passport.authenticate('jwt', {session: false}),controller.create);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), controller.update);
 
 module.exports = router

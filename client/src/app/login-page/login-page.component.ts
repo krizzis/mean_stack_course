322ca2d448223693760ee95f04e6f40643f8c5fa
Slava Belliaev <Viacheslav.Beliaev@workflowwise.com>
2020-11-01 14:28:25 +0200

    42 - Auth service

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,26 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup;
+  aSub: Subscription;
 
-  constructor() { }
+  constructor(private auth: AuthService, 
+              private router: Router,
+              private route: ActivatedRoute) { 
+
+              }
 
   ngOnInit(): void {
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)])
     })
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['registered']) {
+        // Now you can sign in
+      }
+      else if (params['accessDenied']) {
+        // You need to register first
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.aSub)
+      this.aSub.unsubscribe();
   }
 
   onSubmit() {
-    
+    this.form.disable();
+
+    this.aSub = this.auth.login(this.form.value).subscribe(
+      () => this.router.navigate(['/overview']),
+      error => {
+        console.warn(error);
+        this.form.enable;
+      }
+    );
   }
 
 }

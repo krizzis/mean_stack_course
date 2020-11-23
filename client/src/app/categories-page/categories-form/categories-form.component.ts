ca98a7b292f747dce51b23e6c8ac8c89ccd52a98
Slava Belliaev <Viacheslav.Beliaev@workflowwise.com>
2020-11-23 16:10:41 +0200

    54-categories-page-part-2

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { MaterialService } from 'src/app/shared/classes/material.service';
+import { CategoriesService } from 'src/app/shared/services/categories.service';
 
 @Component({
   selector: 'app-categories-form',
@@ -8,22 +13,50 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class CategoriesFormComponent implements OnInit {
 
-  isNew = true;
+  form: FormGroup;
+  isNew = true;  
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private categoriesService: CategoriesService) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
-      console.log('isnew: ', this.isNew)
-      if (params['id']) {
-        // edit
-        this.isNew = false;
-      }
-      else
-      {
-        // create
-      }
+    
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required)
     })
+
+    this.form.disable(); // Comment for BUG - form editing while loaded
+
+    this.route.params
+      .pipe(
+        switchMap(
+          (params: Params) => {
+            if (params['id']) {
+              this.isNew = false; // Comment for BUG - Breadcrums title
+              return this.categoriesService.getById(params['id'])
+            }
+
+            return of(null)
+          }
+        )
+      )
+      .subscribe(
+        category => {
+          if (category) {
+            this.form.patchValue({
+              name: category.name
+            })
+            MaterialService.updateTextInputs() // Comment for BUG - name over placeholder
+          }
+
+          this.form.enable()
+        },
+        error => MaterialService.toast(error.error.message)
+      )
+  }
+
+  onSubmit() {
+
   }
   
 

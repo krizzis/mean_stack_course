d35fd26bffd396fef208223a58c329a73a522db9
Slava Belliaev <Viacheslav.Beliaev@workflowwise.com>
2020-11-23 23:47:13 +0200

    57-add-edit-position-part-1

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -1,18 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MaterialInstance, MaterialService } from 'src/app/shared/classes/material.service';
+import { PositionsService } from 'src/app/shared/services/positions.service';
 
 @Component({
   selector: 'app-positions-form',
   templateUrl: './positions-form.component.html',
   styleUrls: ['./positions-form.component.css']
 })
-export class PositionsFormComponent implements OnInit {
+export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy {
+  @Input("categoryId") categoryId: string;
+  @ViewChild("modal") modalRef: ElementRef;
+  positions: Position[] = [];
+  loading = false;
+  modal: MaterialInstance
 
-  constructor() { }
+  constructor(private positionsService: PositionsService) { }
 
   ngOnInit(): void {
-    
+      this.loading = true;
+      this.positionsService.fetch(this.categoryId)
+        .subscribe(positions => {
+            this.positions = positions;
+            this.loading = false;
+        }) 
   }
   
+  ngOnDestroy() {
+    this.modal.destroy()
+  }
 
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef)
+  }
+
+  onSelectPosition(position: Position) {
+    this.modal.open();
+  }
+
+  onAddPosition() {
+    this.modal.open();
+  }
+
+  onCancel() {
+    this.modal.destroy();
+  }
 }

a593016a9c7b125f0d95fa11b52816329f1943a6
Slava Belliaev <Viacheslav.Beliaev@workflowwise.com>
2020-11-23 12:13:24 +0200

    50-add-pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnalyticsPageComponent } from './analytics-page/analytics-page.component';
+import { CategoriesPageComponent } from './categories-page/categories-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { OverviewComponent } from './overview/overview.component';
+import { OrderPageComponent } from './order-page/order-page.component';
+import { OverviewPageComponent } from './overview-page/overview-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { AuthGuard } from './shared/classes/auth.guard';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
@@ -17,7 +21,11 @@ const routes: Routes = [
     },
     {
         path: '', component: SiteLayoutComponent,canActivate: [AuthGuard], children: [
-            {path: 'overview', component: OverviewComponent}
+            {path: 'overview', component: OverviewPageComponent},
+            {path: 'analytics', component: AnalyticsPageComponent},
+            {path: 'history', component: HistoryPageComponent},
+            {path: 'order', component: OrderPageComponent},
+            {path: 'categories', component: CategoriesPageComponent}
         ]
     }
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,7 +10,11 @@ import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.co
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { TokenInterceptor } from './shared/classes/token.interceptor';
-import { OverviewComponent } from './overview/overview.component';
+import { AnalyticsPageComponent } from './analytics-page/analytics-page.component';
+import { OverviewPageComponent } from './overview-page/overview-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { OrderPageComponent } from './order-page/order-page.component';
+import { CategoriesPageComponent } from './categories-page/categories-page.component';
 
 
 @NgModule({
@@ -20,7 +24,11 @@ import { OverviewComponent } from './overview/overview.component';
     AuthLayoutComponent,
     SiteLayoutComponent,
     RegisterPageComponent,
-    OverviewComponent
+    AnalyticsPageComponent,
+    OverviewPageComponent,
+    HistoryPageComponent,
+    OrderPageComponent,
+    CategoriesPageComponent
   ],
   imports: [
     BrowserModule,
